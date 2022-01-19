0521a20ce0eada54e196d12e6b6a5af8c98388b4
jeevan <jeevanjnayak@gmail.com>
2022-01-19 18:21:39 +0530

    starting the game

---
+++ SnakeLadderStart.java
@@ -0,0 +1,8 @@
+public class SnakeLadderStart {
+
+    public static void main(String[] args) {
+        int position = 0;
+        System.out.println("welcome player number 1");
+        System.out.println("you are at position: "+position);
+    }
+}
